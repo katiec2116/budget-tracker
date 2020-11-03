8b3d9307ba85fbfaea6fe055e28aa24fd70216d1
katiec <katiecampbell21@outlook.com>
2020-11-03 09:32:14 -0500

    html route

--- routes/api.js
+++ routes/api.js
@@ -32,4 +32,9 @@ router.get("/api/transaction", (req, res) => {
     });
 });
 
+
+router.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../index.html"));
+});
+
 module.exports = router;
